8c1b26f6ee2e31d8018979fd0317340fac044c44
Yana <nanaha28@gmail.com>
2023-07-04 13:25:30 +0300

    first commit

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+console.log(1);
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
